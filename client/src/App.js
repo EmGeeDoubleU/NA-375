3af1fa5bfe228211781ee7aa93b61c6996bc1bc6
Michael Grant Warshowksy <mwarshowsky71@gmail.com>
2025-07-29 14:07:25 -0400

    Add React client files properly

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>NA-375 React App</h1>
+        <p>Ready for development!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
